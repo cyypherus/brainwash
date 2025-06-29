bdda13a6ff32a3ec09c3204e670cf1f8dacc9b12
ejjonny <ejjonny@gmail.com>
2025-06-29 14:45:20 -0600

    add some wavetable code for the future

--- benches/oscillator_bench.rs
+++ benches/oscillator_bench.rs
@@ -2,12 +2,37 @@ use brainwash::*;
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 
 fn bench_oscillator_basic_usage(c: &mut Criterion) {
-    c.bench_function("oscillator_basic_usage", |b| {
-        let mut signal = Signal::new(44100);
+    let mut signal = Signal::new(44100);
+    c.bench_function("oscillator_sin_usage", |b| {
         b.iter(|| {
             let mut sin = Osc::default();
             let osc = sin
                 .wave(Wave::Sine)
+                .pitch(black_box(60.0))
+                .freq(black_box(440.0));
+            let output = osc.output(&mut signal);
+            signal.advance();
+            black_box(output);
+        });
+    });
+    let mut signal = Signal::new(44100);
+    c.bench_function("oscillator_basic_usage", |b| {
+        b.iter(|| {
+            let mut sin = Osc::default();
+            let osc = sin
+                .wave(Wave::Square)
+                .pitch(black_box(60.0))
+                .freq(black_box(440.0));
+            let output = osc.output(&mut signal);
+            signal.advance();
+            black_box(output);
+        });
+    });
+    c.bench_function("oscillator_basic_usage", |b| {
+        b.iter(|| {
+            let mut sin = Osc::default();
+            let osc = sin
+                .wave(Wave::Triangle)
                 .pitch(black_box(60.0))
                 .freq(black_box(440.0));
             let output = osc.output(&mut signal);
